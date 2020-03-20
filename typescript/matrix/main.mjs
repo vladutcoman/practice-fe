7e9062e2e5af78e4eb45c22efbaf8840ded881e0
Andrei Vladut Coman <andrei-vladut.coman@ionos.com>
2020-03-20 17:57:00 +0200

    spiral-print

--- typescript/matrix/main.mjs
+++ typescript/matrix/main.mjs
@@ -1,11 +1,13 @@
 import { printMatrix } from './modules/matrix-print.mjs';
 import { rotateMatrix } from './modules/rotate-matrix.mjs';
+import { spiralPrint } from './modules/spiral-print.mjs';
 
 /** Consts */
 const matrix = [[]];
+
+let index = 1;
 for(let i = 0; i < 5; i++) {
   matrix[i] = [];
-  let index = 1;
   for (let j = 0; j < 5; j++) {
     matrix[i][j] = index;
     index++;
@@ -14,8 +16,11 @@ for(let i = 0; i < 5; i++) {
 printMatrix(matrix);
 
 /** Rotate Matrix */
-rotateMatrix(matrix);
-console.log('------- Result Rotate ------');
-printMatrix(matrix);
+// rotateMatrix(matrix);
+// console.log('------- Result Rotate ------');
+// printMatrix(matrix);
 
+/** Spiral print */
+console.log('------- Result Spiral Print ------');
+spiralPrint(matrix);
 
